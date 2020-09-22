6a4d99d8939e455d87ee2cbd077d0ae416039ed2
wadkan <zsoltgaramszegi@gmail.com>
2020-09-22 16:09:11 +0200

    remove after pick up bugfix

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -21,25 +21,22 @@ public abstract class Actor implements Drawable {
 
         try {
             // check tile name: skeleton, sword, key...
-            tileName = nextCell.getActor().getTileName();
             this.actualItem = nextCell.getActor();
-            System.out.println("act: " + actualItem);
-            System.out.println(tileName);
         } catch (Exception ignored) {
         }
 
         // if tile is a wall, do not move
-        if (nextCell.getTileName() != "wall") {
+        if (!nextCell.getTileName().equals("wall")) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
         }
 
-        if (tileName == "skeleton") {   // if tile is a skeleton: FIGHT TODO: implement fight
-            System.out.println("SKELETON");
-        } else if (tileName == "sword" || tileName == "key") {   // if tile is an item (sward, key...): CAN PUT IT TODO put an item
-            cell.setType(CellType.FLOOR);
-        }
+//        if (tileName.equals("skeleton")) {   // if tile is a skeleton: FIGHT TODO: implement fight
+//            System.out.println("SKELETON");
+//        } else if (tileName == "sword" || tileName == "key") {   // if tile is an item (sward, key...): CAN PUT IT TODO put an item
+//            cell.setType(CellType.FLOOR);
+//        }
     }
 
     public int getHealth() {
